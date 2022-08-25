1919c50822d5b2581648c05dc67cbacb2777b2d7
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-08-25 11:14:34 +0300

    add option version

---
+++ newScript.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        buildToDo(e.target.new_todo.value)
+        form.reset()
+    })
+})
+
+function buildToDo(todo) {
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = 'x'
+    p.textContent = `${todo}  `
+    p.appendChild(btn)
+    console.log(p)
+    document.querySelector('#todo_container').appendChild(p)
+}
+
+function handleDelete(e) {
+    e.target.parentNode.remove()
+}
